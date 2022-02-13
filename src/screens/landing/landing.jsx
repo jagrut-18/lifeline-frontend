5d3ebd4e27ef51207beb003bc5349d45c7859479
Kshitij Wagh <kshitij@Kshitijs-MacBook-Pro.local>
2022-02-12 19:29:17 -0500

    #Kshitij: changes on landing screen and index.html

--- src/screens/landing/landing.jsx
+++ src/screens/landing/landing.jsx
@@ -6,24 +6,23 @@ import { useEffect } from 'react';
 function LandingScreen() {
     const axios = require('axios');
 
-    useEffect(() => {
-        axios.get('https://data.cityofnewyork.us/api/views/kk4q-3rt2/rows.json')
-            .then(function (response) {
-                console.log(response);
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            })
-            .then(function () {
-                // always executed
-            });
-    })
+    // useEffect(() => {
+    //     axios.get('https://data.cityofnewyork.us/api/views/kk4q-3rt2/rows.json')
+    //         .then(function (response) {
+    //             console.log(response);
+    //         })
+    //         .catch(function (error) {
+    //             // handle error
+    //             console.log(error);
+    //         })
+    //         .then(function () {
+    //             // always executed
+    //         });
+    // })
 
 
     return (
         <div style={Styles.container}>
-            <Button variant="contained">Hello World</Button>
         </div>
     )
 }
--- src/screens/landing/styles.jsx
+++ src/screens/landing/styles.jsx
@@ -1,9 +1,14 @@
 import COLORS from '../../themes/colors';
+import LandingImage from '../../images/landing_image.svg';
 
 export default {
     container: {
-        backgroundColor: COLORS.background,
-        height: 800,
-        width: 400
+        backgroundImage: `url(${LandingImage})`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "contain",
+        backgroundColor: '#ffffff',
+        width: '100vw',
+        height: '100vh'
     }
 }
