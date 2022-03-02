8f59e455bb4a9e93a23e8cba2a56fbbc28221c97
jagchau <jagchau@iu.edu>
2022-03-02 13:50:46 -0500

    #Jagrut: Cleaned Login.jsx

--- src/screens/login/login.jsx
+++ src/screens/login/login.jsx
@@ -11,9 +11,11 @@ import Google from '../../components/google/google';
 import { useNavigate } from 'react-router-dom';
 import ErrorComponent from '../../components/error/error';
 import routes from '../../routing/routes';
-import axios from 'axios';
 import saveLoginDetails from '../../auth/login';
 import { LoginStateContext } from '../../contexts';
+import { regexEmail, regexPassword } from '../../utilities/regex.js';
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
+import { API } from '../../api/api';
 
 function LoginScreen() {
     const [email, setEmail] = useState('');
@@ -21,77 +23,19 @@ function LoginScreen() {
     const [showPassword, setShowPassword] = useState(false);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
-    const {isLoggedIn, setIsLoggedIn} = useContext(LoginStateContext);
+    const {_, setIsLoggedIn} = useContext(LoginStateContext);
     const navigate = useNavigate();
 
     //validate input
     function validate() {
-        let errorString = ""
-        let regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        let regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
-
-        if (email == "" || !regexEmail.test(email)) {
+        if (!regexEmail.test(email)) {
             setError("Invalid Email")
             return false
         }
 
-        let validationCount1 = 0//for 8 or more characters
-        let validationCount2 = 0//lowercase letters
-        let validationCount3 = 0//upper letters
-        let validationCount4 = 0//At least one number
-        let validationCount5 = 0//At least one specal character
-
-        for (let i = 0; i < password.length; i++) {
-
-            validationCount1++//for 8 characters
-
-            if ((password.charCodeAt(i) >= 65 && password.charCodeAt(i) <= 90) || (password.charCodeAt(i) >= 97 && password.charCodeAt(i) <= 122)) {
-                //for uppercase
-                if (password.charCodeAt(i) >= 65 && password.charCodeAt(i) <= 90) {
-                    validationCount2++
-                }
-
-                //for lowercase
-                if (password.charCodeAt(i) >= 97 && password.charCodeAt(i) <= 122) {
-                    validationCount3++
-                }
-            }
-
-            if (password.charCodeAt(i) >= 48 && password.charCodeAt(i) <= 57) {//for numbers
-                validationCount4++
-            }
-
-            if ((password.charCodeAt(i) >= 33 && password.charCodeAt(i) <= 38) || password.charAt(i) == '@') {//for special characters
-
-                validationCount5++
-            }
-        }
-
-        //Upper and lower letters, At least one number, At least one special character
-
-        //for 8 characters
-        if (validationCount1 < 8) {
-            errorString += "8 or more characters required. "
-        }
-
-        //for lower case and uppercase
-        if (validationCount2 == 0 || validationCount3 == 0) {
-            errorString += "Upper and lower letters required.  "
-        }
-
-        //for numbers
-        if (validationCount4 == 0) {
-            errorString += "At least one number required.  "
-        }
-
-        //for special characters
-        if (validationCount5 == 0) {
-            errorString += "At least one special character required.  "
-        }
-
         //password validation
-        if (password == "" || !regexPassword.test(password)) {
-            setError(errorString)
+        if (!regexPassword.test(password)) {
+            setError("Password should have:\n· 8 or more characters required\n· Upper and lower letters required\n· At least one number required\n· At least one special character required")
             return false
         }
 
@@ -100,34 +44,24 @@ function LoginScreen() {
         return true
     }
 
-    function onNext() {
+    async function onNext() {
         if (!validate()) return;
         setLoading(true);
         var formData = new FormData();
         formData.append('email', email);
         formData.append('password', password);
-
-        axios.post('http://3.220.183.182:5000/login', formData).then(function (response) {
-
-            //for successful login
-            if (response.data.response_code == "200") {
-                saveLoginDetails(email, response.data.data.user_id, response.data.data.token);
-                setIsLoggedIn(true);
-                navigate(routes.home, { replace: true });
-            } else if (response.data.response_code == "220") {
-                //for incorrect password or email
-                setLoading(false);
-                setError("Incorrect password or email")
-            } else if (response.data.response_code == "230") {
-                //for error
-                setLoading(false);
-                setError("Someting went wrong")
-            }
-        })
-            .catch(function (error) {
-                setError("Someting went wrong")
-                setLoading(false);
-            })
+        
+        const response = await API.login(formData);
+        if (response.success) {
+            saveLoginDetails(email, response.data.user_id, response.data.token);
+            setIsLoggedIn(true);
+            navigate(routes.home, { replace: true });
+            setLoading(false);
+        }
+        else {
+            setLoading(false);
+            setError(response.error);
+        }
     }
 
     return (
@@ -139,7 +73,16 @@ function LoginScreen() {
                 <div className="form">
                     <Textfield placeholder="Email address" value={email} onChange={setEmail} />
                     <Spacer height={15} />
-                    <Textfield type={showPassword ? "text" : "password"} placeholder="Password" value={password} onChange={setPassword} />
+                    <div className="password_container">
+                        <Textfield type={showPassword ? "text" : "password"} placeholder="Password" value={password} onChange={setPassword} />
+                        <div className="password_icon" onClick={() => setShowPassword(!showPassword)}>
+                            {
+                                showPassword
+                                ? <AiFillEyeInvisible color='var(--text-secondary)' size={20}/>
+                                : <AiFillEye color='var(--text-secondary)' size={20}/>
+                            }
+                        </div>
+                    </div>
                     <Spacer height={7} />
                     <ForgotPassword />
                     <Button text="Next" onClick={onNext} isLoading={loading}/>
