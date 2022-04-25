c215c94465436335d09b70841bf7bdc72c016d79
jagchau <jagchau@iu.edu>
2022-04-24 21:28:43 -0400

    #Jagrut: home strings

--- src/components/homepatient/homepatient.jsx
+++ src/components/homepatient/homepatient.jsx
@@ -19,8 +19,8 @@ function HomePatient() {
 
     return (
         <div className="row-boxes">
-            <Box onClick={onClick} routeName={"pat_book_appointment"} image={Doctor} header={"Doctors"} description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard"} buttonText={"Find Appointments"} />
-            <Box onClick={onClick} routeName={"search_package_patient"} image={Insurance} header={"Insurance Packages"} description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard"} buttonText={"Explore Plans"} />
+            <Box onClick={onClick} routeName={"pat_book_appointment"} image={Doctor} header={"Doctors"} description={"Book appointments with doctors in simple and easy steps. You can now chat with them anytime & anywhere."} buttonText={"Find Appointments"} />
+            <Box onClick={onClick} routeName={"search_package_patient"} image={Insurance} header={"Insurance Packages"} description={"Get yourself an insurance package and save money with these amazing deals."} buttonText={"Explore Plans"} />
             {/* <div className="box-display-none" /> */}
         </div>
     )
