82e5b02168da9fc504b2d983f666a89223617812
Kshitij Wagh <kshitijwagh@Kshitijs-MacBook-Pro.local>
2022-04-25 19:07:52 -0400

    #Kshitij

--- src/components/homepatient/homepatient.jsx
+++ src/components/homepatient/homepatient.jsx
@@ -20,7 +20,7 @@ function HomePatient() {
     return (
         <div className="row-boxes">
             <Box onClick={onClick} routeName={"pat_book_appointment"} image={Doctor} header={"Doctors"} description={"Book appointments with doctors in simple and easy steps. You can now chat with them anytime & anywhere."} buttonText={"Find Appointments"} />
-            <Box onClick={onClick} routeName={"search_package_patient"} image={Insurance} header={"Insurance Packages"} description={"Get yourself an insurance package and save money with these amazing deals."} buttonText={"Explore Plans"} />
+            <Box onClick={onClick} routeName={"search_package_patient"} image={Insurance} header={"Insurance Packages"} description={"Get yourself an insurance package and save money with these amazing deals. Find yourself the best plan right away."} buttonText={"Explore Plans"} />
             {/* <div className="box-display-none" /> */}
         </div>
     )
